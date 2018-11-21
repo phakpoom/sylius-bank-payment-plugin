76290aa99e0564dad51ef3bb90e48951eaa35b58
liverbool <nukboon@gmail.com>
2018-11-21 17:03:07 +0700

    Update ApplyPaymentListener.php

--- src/EventListener/ApplyPaymentListener.php
+++ src/EventListener/ApplyPaymentListener.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace PhpMob\SyliusBankPaymentPlugin\EventListener;
 
+use Payum\Offline\Constants;
 use PhpMob\SyliusBankPaymentPlugin\Model\TransactionInterface;
 use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\PaymentInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use Sylius\Component\Payment\Factory\PaymentFactoryInterface;
 
@@ -49,8 +51,12 @@ final class ApplyPaymentListener
         }
 
         $order->addPayment(
+            /** @var PaymentInterface $payment */
             $payment = $this->paymentFactory->createWithAmountAndCurrencyCode($subject->getAmount(), $order->getCurrencyCode())
         );
+
+        $payment->setState(PaymentInterface::STATE_NEW);
+        $payment->setDetails([Constants::FIELD_PAID => false, Constants::FIELD_STATUS => Constants::STATUS_PENDING]);
 
         $subject->setPayment($payment);
     }
